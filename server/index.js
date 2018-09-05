b6bb78311ec8a7298e489762ee9fb7628c7eeb41
Alex <aberman12@yahoo.com>
2018-09-05 14:54:44 -0700

    proxy rendering localhost

--- server/index.js
+++ server/index.js
@@ -1,29 +1,63 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
+var request = require('request');
+var helmet = require('helmet');
+var path = require('path')
 
-var app = express();
+var proxy = express();
+var port = 3002;
+proxy.use(helmet());
+proxy.use(bodyParser.json());
+proxy.use(bodyParser.urlencoded({extended:true}));
+proxy.use(express.static(path.join(__dirname, '../')));
 
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+// app.get('/items', function (req, res) {
+//   items.selectAll(function(err, data) {
+//     if(err) {
+//       res.sendStatus(500);
+//     } else {
+//       res.json(data);
+//     }
+//   });
+// });
 
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
-
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
+proxy.use('/items', (req, res) => {
+  request(
+    'http://localhost:3001/items', 
+    (error, response, body) => {
+      if(response.statusCode === 200) {
+        res.status(200).send(body);
+      }
     }
-  });
-});
+  )
+})
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+proxy.use('/api/pageDetails/data', (req, res) => {
+  request('http://localhost:3018/api/pageDetails/data', (error, response, body) => {
+    if(response.statusCode === 200) {
+      res.status(200).send(body);
+    }
+  })
+})
+proxy.use('/api/amenities', (req, res) => {
+  request('http://localhost:3012/api/amenities', (error, response, body) => {
+    if(error){
+      console.log('error in index.js', error)
+    }
+    if(response.statusCode === 200) {
+      res.status(200).send(body);
+    }
+  })
+})
+proxy.use('/api/rooms/1', (req, res) => {
+  request('http://localhost:9001/api/rooms/1', (error, response, body) => {
+    if(response.statusCode === 200) {
+      res.status(200).send(body);
+    }
+  })
+})
+
+proxy.listen(port, function() {
+  console.log(`listening on port ${port}!`);
 });
 
